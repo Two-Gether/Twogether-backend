cfc730fe04c6dd5177167a289c5c14ab8a3e6240
gwony <moonsky1124@naver.com>
2025-06-26 20:52:49 +0900

    Feat: TD-13 ServiceException 도입 및 커스텀 예외 구조 설계
    - ErrorCode 기반 도메인 예외 처리 클래스 구현
    - 메시지 프로퍼티 코드와 HTTP 상태를 함께 관리
    - 비즈니스 예외 발생 시 명확한 응답 구조 제공을 위한 기반 클래스

---
+++ src/main/java/com/yeoro/twogether/global/exception/ServiceException.java
@@ -0,0 +1,20 @@
+package com.yeoro.twogether.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ServiceException extends RuntimeException{
+    private final ErrorCode errorCode;
+    private final String messageCode; // 프로퍼티 메시지
+
+    public ServiceException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.messageCode = errorCode.getMessageCode();
+    }
+
+    public ServiceException(ErrorCode errorCode, Throwable cause) {
+        super(cause);
+        this.errorCode = errorCode;
+        this.messageCode = errorCode.getMessageCode();
+    }
+}
