6ae93e3811c7ec0ca9af02aa8c6720b05543bacb
gwony <moonsky1124@naver.com>
2025-07-10 15:48:48 +0900

    Feat: TD-15 Member DTO 추가

---
+++ src/main/java/com/yeoro/twogether/domain/member/dto/KakaoProfile.java
@@ -0,0 +1,35 @@
+package com.yeoro.twogether.domain.member.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class KakaoProfile {
+
+    private Long id;
+
+    @JsonProperty("kakao_account")
+    private KakaoAccount kakaoAccount;
+
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class KakaoAccount {
+        private String email;
+        private Profile profile;
+    }
+
+    @Getter
+    @Setter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Profile {
+        private String nickname;
+
+        @JsonProperty("profile_image_url")
+        private String profileImageUrl;
+    }
+}
